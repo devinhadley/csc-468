043d2d1047bebaac35bb5295a9550036064d3e32
Devin Hadley <68879608+devinhadley@users.noreply.github.com>
2026-01-20 13:22:35 -0800

    fix jsonl formatting & more approach comments

--- aries.py
+++ aries.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 # 1.) Analysis
 # - We first reconstruct the transaction and dirty page table to determine which transactions were commited and not commited.
 # - Commited transactions are winners, and non-commited transactions are losers.
@@ -20,6 +22,15 @@
 # Undo: Scan the WAL backward and undo the effects of uncommitted (loser)
 # transactions.
 
+# High level understanding: The DBMS takes charge of managing memory in entirety.
+# This means it may flush to disk before a commit (steal) and may not flush to disk
+# following a commmit (no-force). Thus, if there is a failure we need to restore
+# the disk to a known state, and then undo the uncommited changes
+
+_BEGIN_WAL_PROTO = {
+    "LSN": None,
+}
+
 
 def redo():
     pass
@@ -30,7 +41,31 @@ def undo():
 
 
 def analysis():
+    dirty_page_table = defaultdict(int)
+    transaction_table = defaultdict(dict)
+
+    # Goals:
+    # - Construct dirty page table.
+    # - Construct transaction table.
+
+    # 1.) Scan from the bottom up and determine the latest checkpoint.
+    # 2.) Initialize the tables from the checkpoint.
+    # 3.) Then scan back down to update the tables to latest state.
+
     return
+
+
+# Expected Output
+#
+#    Print winner and loser transactions.
+#
+#    Print TT and DPT after Analysis.
+#
+#    Print which updates are redone (by LSN).
+#
+#    Print which updates are undone (by LSN).
+#
+#    Output final disk_pages_after.json representing disk state after recovery (value + pageLSN per page).
 
 
 def main():
